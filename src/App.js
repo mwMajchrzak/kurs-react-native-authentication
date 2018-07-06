923262862344aea77a457a884304b3bba2199e60
mwMajch <mw.majch@gmail.com>
2018-07-06 11:28:49 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/loginForm.js';
 
 class App extends Component {
+    state = { loggedIn: null }
+
     componentWillMount() {
         firebase.initializeApp ({
                 apiKey: "AIzaSyCUU6Lwu6TO7tF3Ss_w3Rg4E2LeVHcnc-M",
@@ -14,12 +16,42 @@ class App extends Component {
                 storageBucket: "react-native-project-auth.appspot.com",
                 messagingSenderId: "630596402072"
         });
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({ loggedIn: true })
+            }
+            else {
+                this.setState({ loggedIn: false })
+            }
+        });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                 return (
+                     <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                             Log out 
+                        </Button>  
+                     </CardSection>  
+                 );
+            case false:
+                return <LoginForm />;
+            default:
+                return ( 
+                    <CardSection>
+                      <Spinner size="large" />
+                    </CardSection>
+                 );
+        }
+    }
+
     render () {
         return (
             <View>
-                <Header headerText="authintication" />
-                <LoginForm/>
+                <Header headerText="authentication" />
+                {this.renderContent()}
             </View>
         );
     }
--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection, Input } from './common';
+import { Text } from 'react-native';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+import firebase from 'firebase';
 
 
 class LoginForm extends React.Component {
-state = { email: '', password: '' };
+state = { email: '', password: '', error: '', loading: false };
 
+onButtonPress()  {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true});
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(this.onLoginSuccess.bind(this))
+    .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(this.onLoginFail.bind(this));
+    });
+
+}
+
+onLoginSuccess() {
+    alert('ba')
+    this.setState({
+        email: '',
+        password: '',
+        loading: false,
+        error: ''
+    });
+}    
+
+onLoginFail() {
+    this.setState({ error: 'Authentication failed.', loading: false});
+}    
+
+renderButton() {
+    if (this.state.loading) {
+        return <Spinner size="small" />;
+    }
+    return (
+        <Button onPress={this.onButtonPress.bind(this)}>
+            Log In
+        </Button>
+    );
+};
 
     render () {
         return (
@@ -28,15 +68,24 @@ state = { email: '', password: '' };
                     onChangeText={password => this.setState({ password })}
                     />
                 </CardSection>    
-
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>    
                 <CardSection>
-                    <Button>
-                        BLA
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>   
 
             </Card>    
-        );
+        );  
     }
+}    
+
+const styles= {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+
 }
 export default LoginForm;
